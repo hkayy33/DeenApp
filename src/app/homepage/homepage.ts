51b3c3ce414a91cf07797f17fb5e1a7bee570f2b
hkayy33 <hkayy33@gmail.com>
2025-12-31 23:50:23 +0000

    feat(): hlink to find tutor button homepage

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Carousel } from './carousel/carousel';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CarouselCardGroup } from './carousel/carousel-card-group/carousel-card-group';
 import { PageContainer } from '../shared/page-container/page-container';
 import { PageContentSection } from '../shared/page-content-section/page-content-section/page-content-section';
@@ -8,7 +8,7 @@ import { PageContentSection } from '../shared/page-content-section/page-content-
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [Carousel, RouterOutlet, CarouselCardGroup,PageContainer, PageContentSection],
+  imports: [Carousel, RouterOutlet, CarouselCardGroup,PageContainer, PageContentSection,RouterLink],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss',
 })
