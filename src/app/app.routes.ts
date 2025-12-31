2979783f3644b55ffc689e04331418f408dadce2
hkayy33 <hkayy33@gmail.com>
2025-12-31 18:55:21 +0000

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { FindTutorDashboard } from './find-tutor/find-tutor-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Homepage },
+  { path: 'find-tutor', component: FindTutorDashboard },
+];
+
